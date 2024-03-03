32483fe52170347975404eb62cdc425a98b1d609
Deepak <deepakramani65@gmail.com>
2024-03-03 16:09:15 +0530

    Initial commit

---
+++ 01 Calculator/script.js
@@ -0,0 +1,25 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+let inputEl = document.querySelector('input');
+inputEl.value = 0;
+var updatedValue = inputEl.value
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '=')
+        {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C' )
+        {   string = "";
+            document.querySelector('input').value = updatedValue;
+        }
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string
+        }
+        
+    })
+})
